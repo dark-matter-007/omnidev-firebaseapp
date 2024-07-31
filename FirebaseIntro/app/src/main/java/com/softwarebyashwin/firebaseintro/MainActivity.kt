f37a636bc6a2b221bc4e297ac611bb7af2ef0253
dark-matter-007 <ashwin.aksharma.p@outlook.com>
2024-07-31 17:04:13 +0530

    add android app as well

---
+++ FirebaseIntro/app/src/main/java/com/softwarebyashwin/firebaseintro/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.softwarebyashwin.firebaseintro
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.database.database
+import com.google.firebase.initialize
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Firebase.initialize(this)
+
+        val myReference = Firebase.database.getReference("/name")
+        val textView = findViewById<TextView>(R.id.textView)
+        myReference.get().addOnSuccessListener {
+            textView.setText(it.value.toString())
+        }.addOnFailureListener{
+            textView.setText("failed to fetch")
+        }
+
+    }
+
+
+}
